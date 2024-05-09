bf0dbeb6f19205a813e693d4f2bee5de43c71132
Ares Ortiz <aortizb597@gmail.com>
2024-05-09 00:49:46 -0600

    pantallas actualizadas
    Se agregó y corrigió la navegación entre pantallas (aún sin lógica).
    Se diseñó toda la pantalla principal

--- App.js
+++ App.js
@@ -1,31 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
-
 import InicioSesion from './componentes/InicioSesion';
+import PantallaPrincipal from './componentes/PantallaPrincipal';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <ImageBackground source={require('./assets/pantInicioSesion.png')} style={styles.backgroundImage}>
-      <View style={styles.container}>
-      <InicioSesion />
-      <StatusBar style="auto" />
-    </View>
-    </ImageBackground>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='InicioSesion'>
+        <Stack.Screen name="InicioSesion" component={InicioSesion} options={{headerShown: false}}/>
+        <Stack.Screen name="PantallaPrincipal" component={PantallaPrincipal} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //backgroundColor: '#AEC8D8',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover', // Opcional: puedes ajustar el modo de ajuste de la imagen de fondo
-    justifyContent: 'center',
-  },
-  
-});
--- componentes/InicioSesion.js
+++ componentes/InicioSesion.js
@@ -1,47 +1,67 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, Alert, ImageBackground} from "react-native";
+import { View, Text, TextInput, Button, Alert, ImageBackground, Dimensions, StyleSheet} from "react-native";
 import Boton from "./Boton";
 
-const InicioSesion = () => {
+const InicioSesion = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleLogin = () => {
+    //Aqui vamos a tener que poner toda la logica para validar a un usuario
+    navigation.navigate('PantallaPrincipal')
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <TextInput
-        placeholder="Correo electrónico"
-        //backgroundColor="white"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-        style={{
-          padding: 10,
-          marginBottom: 30,
-          marginTop: 205,
-          width: 300,
-          height: 60,
-          backgroundColor: "white",
-          borderRadius: 20,
-          //marginTop: -225, 
-        }}
-      />
-      <TextInput
-        placeholder="Contraseña"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry={true}
-        style={{
-          padding: 10,
-          marginBottom: 40,
-          marginTop: 15,
-          width: 300,
-          height: 60,
-          backgroundColor: "white",
-          borderRadius: 20,
-        }}
-      />
-      <Boton title="Iniciar sesion" />
-    </View>
+    <ImageBackground source={require('../assets/pantInicioSesion.png')} style={styles.background} resizeMode="cover">
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <TextInput
+          placeholder="Correo electrónico"
+          //backgroundColor="white"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={{
+            padding: 10,
+            marginBottom: 30,
+            marginTop: 205,
+            width: 300,
+            height: 60,
+            backgroundColor: "white",
+            borderRadius: 20, 
+          }}
+        />
+        <TextInput
+          placeholder="Contraseña"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry={true}
+          style={{
+            padding: 10,
+            marginBottom: 40,
+            marginTop: 15,
+            width: 300,
+            height: 60,
+            backgroundColor: "white",
+            borderRadius: 20,
+          }}
+        />
+        <Boton title="Iniciar sesion" onPress={handleLogin}/>
+      </View>
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
 
 export default InicioSesion;
