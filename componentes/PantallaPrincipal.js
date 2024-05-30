bd6232d5eaf5606714dfcb83c0ccbb6746ae59a7
A01754650 <88682624+A01754650@users.noreply.github.com>
2024-05-30 15:28:10 -0600

    PAL ares

--- componentes/PantallaPrincipal.js
+++ componentes/PantallaPrincipal.js
@@ -1,6 +1,16 @@
-import React, { useEffect, useState } from "react";
-import {StyleSheet, View, ImageBackground, Text, Image, Modal, TouchableOpacity, ScrollView, ActivityIndicator,} from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState, useCallback } from "react";
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Text,
+  Image,
+  Modal,
+  TouchableOpacity,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import Boton from "./Boton";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -9,36 +19,113 @@ const PantallaPrincipal = () => {
   const [popupVisible, setPopupVisible] = useState(false);
   const [infotarjetas, setInfoTarjetas] = useState(null);
   const [cargando, setCargando] = useState(true);
+  const [tarjetaSeleccionada, setTarjetaSeleccionada] = useState(null);
+  const [terminacion, setTerminacion] = useState(null);
+  const [saldo, setSaldo] = useState(null); 
+  const [arrayTransacciones, setArrayTransacciones] = useState([]); // [fecha, descripcion, cantidad
+  const [cuentas, setCuentas] = useState(null);
   const navigation = useNavigation();
+  //Definir variables para las transacciones
+  const [nombreTransaccion, setNombreTransaccion] = useState(null);
+  const [montoTransaccion, setMontoTransaccion] = useState(null);
+  const [fechaTransaccion, setFechaTransaccion] = useState(null);
+  const [estatusTransaccion, setEstatusTransaccion] = useState(null);
+  const [idTransaccion, setIdTransaccion] = useState(null);
+  const [detalleTransaccion, setDetalleTransaccion] = useState(null);
+
+  const descargarTransacciones = async (info, lista)  => {
+    try{
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: info,
+      };
+      const response = await fetch(
+        "http://10.48.70.212:8080/transaccion/getTransacciones",
+        requestOptions
+      );
+      const data = await response.json();
+      //por transaccion
+      var temp = []
+      for (var i = 0; i < data.length; i++){
+        temp.push([data[i].detalle, data[i].estatus, data[i].fecha, data[i].idTransaccion, data[i].monto, data[i].nombre]);
+      }
+      lista.push(temp);
+      console.log('Array transacciones:', lista);
+  
+      return lista;
+    } catch (error) {
+      console.error("Error al obtener las transacciones:", error);
+    }
+    
+  };
+
+  const fetchClienteData = async () => {
+    try {
+      const nombre = await AsyncStorage.getItem("nombre");
+      const infotarjetasString = await AsyncStorage.getItem('InfoTarjetas');
+      const cuentas = await AsyncStorage.getItem('cuentas');
+      
+      setNombre(nombre);
+      
+
+      if (infotarjetasString) {
+        const infotarjetas = JSON.parse(infotarjetasString);
+        setInfoTarjetas(infotarjetas);
+        console.log('Array recuperado: ', infotarjetas);
+
+        if (!tarjetaSeleccionada && infotarjetas[0] && infotarjetas[0].length > 0) {
+          setTerminacion(infotarjetas[0][0]); // Establecer la terminación inicial num cuenta
+          setSaldo(infotarjetas[1][0]); // Establecer el saldo inicial
+          await descargarTransacciones(JSON.stringify({numCuenta: infotarjetas[0][0]}), arrayTransacciones)
+          setNombreTransaccion(arrayTransacciones[0][0][5]);
+          setMontoTransaccion(arrayTransacciones[0][0][4]);
+          setFechaTransaccion(arrayTransacciones[0][0][2]);
+          setEstatusTransaccion(arrayTransacciones[0][0][1]);
+          setIdTransaccion(arrayTransacciones[0][0][3]);
+          setDetalleTransaccion(arrayTransacciones[0][0][0]);
+        }
+      }
+
+    } catch (error) {
+      console.error(
+        "Error al obtener los datos del cliente en pantalla:", error);
+    } finally {
+      setCargando(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchClienteData = async () => {
-      try {
-        const nombre = await AsyncStorage.getItem("nombre");
-        const infotarjetasString = await AsyncStorage.getItem('InfoTarjetas');
-
-        setNombre(nombre);
-        console.log("AYUDA", infotarjetasString);
-
-        if(infotarjetasString) {
-          const infotarjetas = JSON.parse(infotarjetasString);
-          setInfoTarjetas(infotarjetas);
-          console.log('Array recuperado: ', infotarjetas);
-        }
-
-      } catch (error) {
-        console.error(
-          "Error al obtener los datos del cliente en pantalla:", error);
-      } finally {
-        setCargando(false);
-      }
-    };
     fetchClienteData();
   }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      if (tarjetaSeleccionada) {
+        fetchClienteData();
+      }
+    }, [tarjetaSeleccionada])
+  );
 
   if (cargando) {
     return <ActivityIndicator size="large" color="#0000ff" />;
   }
+
+  const clearList = () => {
+    setArrayTransacciones([]); // Actualiza el estado a una matriz vacía
+  };
+
+  const handlePress = async (i) => {
+    setPopupVisible(false);
+    setTarjetaSeleccionada(infotarjetas[0][i]);
+    setTerminacion(infotarjetas[0][i]); // Actualizar la terminación
+    setSaldo(infotarjetas[1][i]); // Actualizar el saldo
+    clearList();
+    console.log('Array transacciones en handle:', arrayTransacciones);
+    await descargarTransacciones(JSON.stringify({numCuenta: infotarjetas[0][i]}), arrayTransacciones);
+    console.log('Array transacciones en handle2 :', arrayTransacciones);
+    console.log(`Tarjeta seleccionada: ${infotarjetas[0][i]}`);
+  };
 
   return (
     <ImageBackground
@@ -54,7 +141,10 @@ const PantallaPrincipal = () => {
               Mis tarjetas
             </Text>
             <TouchableOpacity onPress={() => setPopupVisible(true)}>
-              <Text>{}</Text>
+              <Image
+                source={require("../assets/tarjeta.png")}
+                style={styles.image}
+              ></Image>
             </TouchableOpacity>
           </View>
 
@@ -66,11 +156,11 @@ const PantallaPrincipal = () => {
               <View style={styles.transaccion}>
                 <View>
                   <Text style={styles.textMediano}>Terminación</Text>
-                  <Text style={styles.textCantidadCuenta}>{infotarjetas[0][0].toString().slice(-4)}</Text>
+                  <Text style={styles.textCantidadCuenta}>{terminacion}</Text>
                 </View>
                 <View style={styles.right}>
                   <Text style={styles.textMediano}>Saldo disponible</Text>
-                  <Text style={styles.textCantidadCuenta}>{infotarjetas[1][0]}</Text>
+                  <Text style={styles.textCantidadCuenta}>{saldo}</Text>
                 </View>
               </View>
             </View>
@@ -85,11 +175,11 @@ const PantallaPrincipal = () => {
               </Text>
               <View style={styles.transaccion}>
                 <View>
-                  <Text style={styles.text}>18 Mayo 2024</Text>
-                  <Text style={styles.textDescripcion}>Pull bear polanco</Text>
+                  <Text style={styles.text}>{fechaTransaccion}</Text>
+                  <Text style={styles.textDescripcion}>{detalleTransaccion}</Text>
                 </View>
                 <Text style={[styles.textTransaccion, styles.right]}>
-                  $3000
+                  {montoTransaccion}
                 </Text>
               </View>
               <View style={styles.transaccion}>
@@ -117,18 +207,15 @@ const PantallaPrincipal = () => {
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
             <Text style={[styles.titulo, styles.bold, styles.center]}>
-              Tarjeta de crédito
+              Tarjetas
             </Text>
             <ScrollView contentContainerStyle={styles.scrollView}>
-              {[...Array(6)].map((_, i) => (
+              {infotarjetas[0].map((_, i) => (
                 <TouchableOpacity
                   key={i}
-                  onPress={() => setPopupVisible(false)}
+                  onPress={() => handlePress(i)}
                 >
-                  <Image
-                    source={require("../assets/tarjeta.png")}
-                    style={styles.image}
-                  ></Image>
+                  <Text style={styles.text}>{infotarjetas[0][i]}</Text>
                 </TouchableOpacity>
               ))}
             </ScrollView>
